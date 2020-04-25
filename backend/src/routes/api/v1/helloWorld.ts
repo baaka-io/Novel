aa69dfc97fd5a0bc6c5d45a874e5af9e6b4ed681
Tim Untersberger <timuntersberger2@gmail.com>
2020-04-25 14:16:06 +0200

    Please just work (#4)

--- backend/src/routes/api/v1/helloWorld.ts
+++ backend/src/routes/api/v1/helloWorld.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "trainstation";
 
-export const url = "test";
+export const url = "pls";
 
 export default function helloWorld(req: Request, res: Response) {
   res.send("Hello World");
