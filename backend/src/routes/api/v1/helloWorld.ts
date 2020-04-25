a0a8e43d72b72d360225ded42f083dca5d6287b1
Tim Untersberger <timuntersberger2@gmail.com>
2020-04-25 14:20:24 +0200

    It will work 100% :) (#4)

--- backend/src/routes/api/v1/helloWorld.ts
+++ backend/src/routes/api/v1/helloWorld.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "trainstation";
 
-export const url = "pls";
+export const url = "test";
 
 export default function helloWorld(req: Request, res: Response) {
   res.send("Hello World");
