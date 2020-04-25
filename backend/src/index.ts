f865d1c84bf1f206ac47a3743280ede27f10c51f
Tim Untersberger <timuntersberger2@gmail.com>
2020-04-25 12:50:30 +0200

    Implement Dockerfile; Add exit handlers
    Added a signal listener using the death npm package, because the
    application ignored all kill signals.
    Also added a route that returns all registered routes.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
 import { port } from "./environment";
 import logger from "./logger";
 import server from "./server";
+import ON_DEATH from "death";
+
+ON_DEATH((signal) => {
+  logger.info("Received signal " + signal);
+  process.exit(1);
+});
 
 server.listen(port, () => {
   logger.info(`HTTP Server listening on port ${port}`);
